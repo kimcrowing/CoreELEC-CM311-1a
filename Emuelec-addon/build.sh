a8406869129483d5e9ec6d116ec15d17acbb919e
kimcrowing <50307403+kimcrowing@users.noreply.github.com>
2024-03-13 07:46:47 +0800

    Update build.sh

--- Emuelec-addon/build.sh
+++ Emuelec-addon/build.sh
@@ -1,8 +1,11 @@
 
-source_img_url="https://ghproxy.org/https://github.com/CoreELEC/CoreELEC/releases/download/${version}/${source_img_name}.img.gz"
-
+source_url="https://github.com/EmuELEC/EmuELEC.git"
 
 echo " Welcome to build Emulelec-addon""
 echo "Downloading CEmulelec"
-git clone ${source_img_url}
-echo "Copy addon.sh"
+git clone ${source_url}
+echo "Copy emuelec-addon.sh"
+cd Emuelec
+sudo cp Emuelec-addon/emuelec-addon.sh Emuelec/emuelec-addon.sh
+sudo chown -x Emuelec/emuelec-addon.sh
+
