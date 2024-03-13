74e76c61d24e65f6e057444d4a62f7314da27df7
kimcrowing <50307403+kimcrowing@users.noreply.github.com>
2024-03-13 09:04:58 +0800

    Update build.sh

--- Emuelec-addon/build.sh
+++ Emuelec-addon/build.sh
@@ -3,6 +3,7 @@ source_url="https://github.com/EmuELEC/EmuELEC.git"
 echo " Welcome to build Emulelec-addon"
 echo "Downloading CEmulelec"
 git clone https://github.com/EmuELEC/EmuELEC.git
+ls
 echo "Copy emuelec-addon.sh"
 sudo cp ./emuelec-addon.sh ./Emuelec/emuelec-addon.sh
 sudo chown +x ./Emuelec/emuelec-addon.sh
