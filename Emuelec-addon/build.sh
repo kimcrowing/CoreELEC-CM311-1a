6568ab66e2058cdfde0ebb7a312a1e633d0b9f06
kimcrowing <50307403+kimcrowing@users.noreply.github.com>
2024-03-17 12:02:48 +0800

    Update build.sh

--- Emuelec-addon/build.sh
+++ Emuelec-addon/build.sh
@@ -7,7 +7,7 @@ git clone -b dev_4.3 https://github.com/EmuELEC/EmuELEC.git
 ls
 #cd EmuELEC
 #PROJECT=Amlogic-ce DEVICE=Amlogic-ng ARCH=aarch64 DISTRO=EmuELEC make image
-#echo "Copy emuelec-addon.sh"
-#sudo cp ./emuelec-addon.sh ./EmuELEC/
-#sudo chmod a+x ./EmuELEC/emuelec-addon.sh
+echo "Copy emuelec-addon.sh"
+sudo \cp ./emuelec-addon.sh ./EmuELEC/
+sudo chmod a+x ./EmuELEC/emuelec-addon.sh
 
