ddf064916a1c36f79ccacf985d8f61bea12a45a2
kimcrowing <50307403+kimcrowing@users.noreply.github.com>
2024-05-01 13:19:47 +0800

    Update build.sh

--- CM311-1a-CoreELEC/build.sh
+++ CM311-1a-CoreELEC/build.sh
@@ -14,9 +14,9 @@ libreelec_path="${system_root}/usr/lib/libreelec"
 config_path="${system_root}/usr/config"
 firmware_path="${system_root}/usr/lib/kernel-overlays/base/lib/firmware"
 kodi_userdata="${mount_point}/.kodi/userdata"
-emuelec_url="https://github.com/kimcrowing/CoreELEC-CM311-1a/releases/download/V4.3/script.emuelec.Amlogic-ng.launcher-4.3-Amlogic-ng.zip"
+addon_file="script.emuelec.Amlogic-ng.launcher"
 kodi_addon="${mount_point}/.kodi/addons"
-addon_file="script.emuelec.Amlogic-ng.launcher.zip"
+
 
 echo "Welcome to build CoreELEC for CM311-1a!"
 echo "Downloadi CoreELEC-${version} generic image"
@@ -31,11 +31,10 @@ offset=$(($(fdisk -l -o start ${source_img_name}.img|grep -v "[a-zA-Z]"|grep -v 
 sudo mount -o loop,offset=${offset} ${source_img_name}.img ${mount_point}
 
 
-mkdir -p ${kodi_addon}
+mkdir -p ${kodi_addon}/script.emuelec.Amlogic-ng.launcher
 echo "Downloading Emuelec-addon"
-wget ${emuelec_url} -O ${addon_file} | exit 1
 echo "copy emuELEC addon"
-sudo unzip ${addon_file} -d ${kodi_addon} | exit 1
+sudo cp ${addon_file} ${kodi_addon}/script.emuelec.Amlogic-ng.launcher
 
 echo "Copying CM311-1a DTB file"
 sudo cp ${common_files}/cm311-1a.dtb ${mount_point}/dtb.img
